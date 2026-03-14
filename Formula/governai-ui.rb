ceda7393aa8394d834b84900e4b014deaf7183cb
rrrozhd <dondoe@dondoes-MacBook-Air.local>
2026-03-14 10:27:55 +0300

    release: v0.1.0

--- Formula/governai-ui.rb
+++ Formula/governai-ui.rb
@@ -2,7 +2,7 @@ class GovernaiUi < Formula
   desc "GovernAI workflow builder CLI/TUI"
   homepage "https://github.com/rrrozhd/governai-ui"
   url "https://files.pythonhosted.org/packages/source/g/governai-ui/governai_ui-0.1.0.tar.gz"
-  sha256 "4f2371eb56c1d3b65f4f615454288192bbe4445d011cc2b17d8148b838dcb78d"
+  sha256 "117778f2c1d2d8445a4447e89176f1ce13417fe404e36165b87ce44bb1e114bb"
 
   depends_on "python@3.12"
 
